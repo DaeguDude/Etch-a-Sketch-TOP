0f96941ab988666b08881177f7d39b209a76814c
DaeguDude <k3hppk@gmail.com>
2020-02-28 12:05:02 +0900

    Trying to add custom attribute

--- test.js
+++ test.js
@@ -1,28 +1,10 @@
-Math.floor(Math.random() * 256);
+let str = 'rgb(68, 39, 160)';
 
-// Does random return 1 as well? NOP
-let redVal = Math.floor(Math.random() * 256);
-let oneTenthRedVal = (redVal * 0.1).toPrecision(3);
-
-let greenVal = Math.floor(Math.random() * 256);
-let oneTenthGreenVal = (greenVal * 0.1).toPrecision(3);
-
-let blueVal = Math.floor(Math.random() * 256);
-let oneTenthBlueVal = (blueVal * 0.1).toPrecision(3);
-
-for (let i = 0; i < 10; i++) {
-  redVal = (redVal - oneTenthRedVal).toPrecision(4);
-  greenVal = (greenVal - oneTenthGreenVal).toPrecision(4);
-  blueVal = (blueVal - oneTenthBlueVal).toPrecision(4);
-}
-
-console.log(redVal);
-console.log(greenVal);
-console.log(blueVal);
-
-
-
-
+let startIndex = str.search("\\(") + 1;
+let endIndex = str.search("\\)");
+let rgb = str.substring(startIndex, endIndex);
+rgb = rgb.split(",");
+console.log(rgb);
 
 
 for (let i = 0; i < 10; i++) {
